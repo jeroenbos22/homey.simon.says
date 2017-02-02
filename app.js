c97bcf305b46160e98778c2e03d8ff453fa9055d
jeroenbos22 <jeroenbos22@gmail.com>
2017-02-02 18:51:03 +0100

    Final commit

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 "use strict";
 
+var sentence = '';
+
 function init() {
     Homey.log("Homey Simon says app ready!");
 }
 
 Homey.manager('speech-input').on('speech', function (speech) {
     sentence = speech.transcript;
-    Homey.log('Simon says detected');
+    sentence = sentence.replace('simon says','').replace('echo','').trim();
+    
+    Homey.log('Simon says detected: '+sentence);
     
     Homey.manager('speech-output').say(sentence);
-
-    Homey.manager('flow').trigger('simon_says_trigger', tokens, {session: speech.session, sentence:sentence}, function (err, result) {
+    
+    Homey.manager('flow').trigger('simon_says_trigger', {sentence:sentence}, {session: speech.session}, function (err, result) {
         if (err) return Homey.error(err);
-    });    
+    });
 });
 
 module.exports.init = init;
